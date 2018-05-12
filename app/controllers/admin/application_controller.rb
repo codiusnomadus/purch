b2f3dfdda5e735dd7c3faab19aa4c94bb6cbfc6c
thecodenomad@gmail.com <thecodenomad@gmail.com>
2018-05-12 21:11:39 +0800

    updated administrate

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,28 +6,28 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    # before_action :authenticate_user!
-    before_action :authenticate_admin
+    before_action :authenticate_user!
+    # before_action :authenticate_admin
 
     def authenticate_admin
-      not_found unless current_user && access_whitelist
+      redirect_to '/', alert: "You are not allowed to access this page" unless current_user && access_whitelist
     end
 
     private
       def access_whitelist
-        user_signed_in? && (current_user.has_role?(:admin) || current_user.has_role?(:editor))
+        user_signed_in? && (current_user.has_role?(:editor))
       end
 
-      def not_found
-        begin
-          raise ActionController::RoutingError.new('404 - The page you are looking for cannot be found.')
-        rescue
-          render_404
-        end
-      end
+      # def not_found
+      #   begin
+      #     raise ActionController::RoutingError.new('404 - The page you are looking for cannot be found.')
+      #   rescue
+      #     render_404
+      #   end
+      # end
 
-      def render_404
-        render file: "#{Rails.root}/public/404", status: :not_found
-      end
+      # def render_404
+      #   render file: "#{Rails.root}/public/404", status: :not_found
+      # end
   end
 end
