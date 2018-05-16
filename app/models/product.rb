e32c255f2f74337cc0a7f86f8030430dfd408238
thecodenomad@gmail.com <thecodenomad@gmail.com>
2018-05-16 20:59:49 +0800

    updated product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ApplicationRecord
   resourcify
 
   belongs_to :user, dependent: :destroy
+  belongs_to :brand,
+    primary_key: 'code',
+    foreign_key: 'brand_code'
 
   has_many :uploads, as: :uploadable, dependent: :destroy
   accepts_nested_attributes_for :uploads, allow_destroy: true
